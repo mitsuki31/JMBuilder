851e3f907508394ce5ae87f89a98eb5000853dc4
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-07-24 20:07:32 +0700

    Add module docstring for `jm_builder`

--- src/jm_builder/__init__.py
+++ src/jm_builder/__init__.py
@@ -1,3 +1,24 @@
+"""JMBuilder package
+
+JMBuilder or JMatrixBuilder, provides some utilities to build the ``JMatrix``.
+For more information about ``JMatrix``, you can refer to link below.
+
+    https://github.com/mitsuki31/jmatrix.git
+
+Available Classes
+-----------------
+JMException
+    This class is a base exception for this package.
+
+_JMCustomPath
+    This class provides all path variables that used by ``JM Builder`` package.
+    All path variables in this class are read-only properties.
+
+Available Constants
+-------------------
+
+"""
+
 from . import exception
 from .exception import *
 from . import utils
