74f91a04871e855bf87a55eac3fc6c89a0e0310b
Ryuu Mitsuki <117973493+mitsuki31@users.noreply.github.com>
2023-09-14 22:19:18 +0700

    bugfix: Fix imports error in Python 3.7
    * Addressed the imports error when using Python 3.7 due to uncatched
      an exception called `ValueError`.
    * Added new parameter to `__print_version` called `file`, which can redirect
      the printed version info to specific opened file. Default is `sys.stdout`.
    * Now the main driver supported new argument, it is `-version` that will
      makes the output of version info redirected to standard error (`sys.stderr`).
    
        - `-V`, `--version` -> standard output
        - `-version`        -> standard error

--- jmbuilder/__main__.py
+++ jmbuilder/__main__.py
@@ -7,13 +7,13 @@ Copyright (c) 2023 Ryuu Mitsuki.
 import os as __os
 import sys as __sys
 from pathlib import Path as __Path
-from typing import Any, Union
+from typing import Any, Union, TextIO
 
 
 try:
     from _globals import AUTHOR
     from utils import config as __config
-except (ImportError, ModuleNotFoundError):
+except (ImportError, ModuleNotFoundError, ValueError):
     # Add a new Python search path to the first index
     __sys.path.insert(0, str(__Path(__sys.path[0]).parent))
     del __Path  # This no longer used
@@ -24,9 +24,9 @@ except (ImportError, ModuleNotFoundError):
 __author__ = AUTHOR
 del AUTHOR
 
-def __print_version(_exit: bool = False) -> None:
+def __print_version(_exit: bool = False, *, file: TextIO = __sys.stdout) -> None:
     """
-    Print the version info to standard output.
+    Print the version info to specific opened file.
 
     Parameters
     ----------
@@ -34,19 +34,26 @@ def __print_version(_exit: bool = False) -> None:
         Whether to exit and terminate the Python after printed the version.
         Defaults to False (disabled).
 
+    file : TextIO, optional
+        The file to print the version info.
+        Defaults to console standard output (`sys.stdout`).
+
     """
+
+    if not file:
+        raise ValueError("File must be an opened file object, not None")
 
     _setupcls: __config._JMSetupConfRetriever = __config.setupinit()
 
     program_name: str = _setupcls.progname
-    version:      str = 'v' + '.'.join(str(v) for v in _setupcls.version)
+    version:      str = 'v{}'.format('.'.join(map(str, _setupcls.version)))
     author:       str = _setupcls.author
 
     print(
         program_name, version,                                 # Program name and version
         __os.linesep + \
         f'Copyright (C) 2023 {author}. All rights reserved.',  # Copyright notice
-        file=__sys.stdout
+        file=file
     )
     if _exit:
         __sys.exit(0)
@@ -59,7 +66,7 @@ def __argchck(targets: Any, args: Union[list, tuple]) -> bool:
     Paramaters
     ----------
     targets : Any
-        An argument or a list of arguments to searched for.
+        An argument or a list of arguments (must iterable) to searched for.
 
     args : list or tuple
         A list of arguments.
@@ -76,7 +83,7 @@ def __argchck(targets: Any, args: Union[list, tuple]) -> bool:
 
     found: bool = False
     for target in targets:
-        if target in args:
+        if str(target) in args:
             found = True
 
     return found
@@ -92,15 +99,25 @@ def main() -> None:
 
     # Check for `-V` or `--version` in the arguments
     # If found, print the version info then exit with exit code zero (success)
+    #
     if __argchck(version_args, args):
-        __print_version(_exit=True)
+        __print_version(True)
+
+    # For `-version` argument, the output will be redirected
+    # to the standard error (`sys.stderr`)
+    #   `-V`, `--version` -> sys.stdout
+    #   `-version`        -> sys.stderr
+    #
+    elif __argchck('-version', args):
+        __print_version(True, file=__sys.stderr)
+
 
     # ... Still in development
 
 
 
 # Delete unnecessary imported objects
-del Any, Union
+del Any, Union, TextIO
 
 
 if __name__ == '__main__':
